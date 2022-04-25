6c2ae62d6c63ce5c1d61b9f01fe1ceed132918ec
jaehyun hwang <93072571+nuyh99@users.noreply.github.com>
2022-04-25 19:25:26 +0900

    change db(h2 to mariaDB)

--- src/main/java/product/MemberManagement/controller/CustomErrorPageController.java
+++ src/main/java/product/MemberManagement/controller/CustomErrorPageController.java
@@ -1,10 +1,11 @@
 package product.MemberManagement.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorPageController {
+public class CustomErrorPageController implements ErrorController {
 
     @RequestMapping("/error")
     public String goHome() {
