b7e85d0dad296e7802e0d16ef1b79d22643f9a8f
jaehyun hwang <93072571+nuyh99@users.noreply.github.com>
2022-04-26 01:21:12 +0900

    final

--- frontend/project/src/index.js
+++ frontend/project/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './routers/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
